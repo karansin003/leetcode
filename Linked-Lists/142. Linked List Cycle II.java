66a2021d7d5c38143efe33cf629a8de0930aba80
Karan Kumar <karansin8672@gmail.com>
2026-06-12 13:01:40 +0530

    Create 142. Linked List Cycle II.java

---
+++ Linked-Lists/142. Linked List Cycle II.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                ListNode pre = head;
+                while (pre != slow) {
+                    pre = pre.next;
+                    slow = slow.next;
+                }
+                return pre;
+            }
+        }
+        return null;
+    }
+}
